10b07edb650cb9b660ec39daa1bff6f614099371
nagalokeshp13 <nagalokeshp13@gmail.com>
2021-04-26 14:26:55 +0530

    initial routes added, email list top section ui completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Mail from './components/Mail';
+import EmailList from './components/EmailList';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/" exact>
+              <EmailList />
+            </Route>
+
+            <Route path="/mail">
+              <Mail />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
